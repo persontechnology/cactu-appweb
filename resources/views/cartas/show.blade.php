784b9f542ecec0d854e4ceb3fe8b7545e5597a60
persontechnology <persontechnologys@gmail.com>
2023-06-29 09:05:59 -0500

    pdf

--- resources/views/cartas/show.blade.php
+++ resources/views/cartas/show.blade.php
@@ -9,28 +9,32 @@
 @section('content')
 <div class="card">
     <div class="card-body">
-        <div class="table-responsive">
-            <div class="ratio ratio-16x9">
-                @can('ver', $carta)
-                    <iframe src="{{ route('cartas.ver-carta-pdf',$carta->id) }}" title="YouTube video" allowfullscreen></iframe>
-                @else
-                <div class="alert alert-primary alert-dismissible fade show" role="alert">
-                  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                  <strong>{{ $carta->ninio->nombres_completos }} no pertenece a usd, o no responde la carta de {{ $carta->tipo }}.</strong> 
-                </div>
-                
-                <script>
-                  var alertList = document.querySelectorAll('.alert');
-                  alertList.forEach(function (alert) {
-                    new bootstrap.Alert(alert)
-                  })
-                </script>
-                
-                @endcan
-                
-              </div>
+        <div class="ratio ratio-16x9">
+            @can('ver', $carta)
+                <iframe src="{{ route('cartas.ver-carta-pdf',$carta->id) }}" title="YouTube video" allowfullscreen></iframe>
+            @else
+            <div class="alert alert-primary alert-dismissible fade show" role="alert">
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              <strong>{{ $carta->ninio->nombres_completos }} no pertenece a usd, o no responde la carta de {{ $carta->tipo }}.</strong> 
+            </div>
+            
+            <script>
+              var alertList = document.querySelectorAll('.alert');
+              alertList.forEach(function (alert) {
+                new bootstrap.Alert(alert)
+              })
+            </script>
+            
+            @endcan
+            
         </div>
     </div>
+</div>
+<a href="{{ asset(Storage::url($carta->archivo_pdf)) }}">{{ asset(Storage::url($carta->archivo_pdf)) }}</a>
+<div class="ratio ratio-16x9">
+  
+<iframe src="{{ asset(Storage::url($carta->archivo_pdf)) }}" title="YouTube video" allowfullscreen></iframe>
+
 </div>
 
 @endsection
