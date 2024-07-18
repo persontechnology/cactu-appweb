137f20bd365c01755c568e5bb672bb32c68a1d5a
persontechnology <persontechnologys@gmail.com>
2024-07-18 09:38:54 -0500

    cartas inicias

--- resources/views/cartas/pdf-contestacion.blade.php
+++ resources/views/cartas/pdf-contestacion.blade.php
@@ -160,7 +160,7 @@
                             background-size: 100% 100%;
                             background-position: center; 
                             width: 50%; 
-                            height: 450px;
+                            height: 550px;
                         "></td>
                         @if ($carta->archivo_familia_ninio)
                             <td style="
@@ -169,7 +169,7 @@
                                 background-size: 100% 100%;
                                 background-position: center; 
                                 width: 50%; 
-                                height: 450px;
+                                height: 550px;
                             "></td>
                         @endif
                     </tr>
